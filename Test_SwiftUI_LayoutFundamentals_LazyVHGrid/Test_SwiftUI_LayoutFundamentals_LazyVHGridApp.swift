fbe8dbf50e2b17e02cb92a571b0130d83ae397a0
JTrojanowicz <jaroslaw.trojanowicz@gmail.com>
2022-10-05 12:47:34 +0200

    Initial Commit

---
+++ Test_SwiftUI_LayoutFundamentals_LazyVHGrid/Test_SwiftUI_LayoutFundamentals_LazyVHGridApp.swift
@@ -0,0 +1,17 @@
+//
+//  Test_SwiftUI_LayoutFundamentals_LazyVHGridApp.swift
+//  Test_SwiftUI_LayoutFundamentals_LazyVHGrid
+//
+//  Created by Jaroslaw Trojanowicz on 05/10/2022.
+//
+
+import SwiftUI
+
+@main
+struct Test_SwiftUI_LayoutFundamentals_LazyVHGridApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
